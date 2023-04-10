1fda6462282877c347b71a68481668c2f9dde409
Your Name <you@example.com>
2023-04-10 21:23:18 +0300

    Final modified:   UACorrectingMachine/templates/UACorrectingMachine/index.html modified:   UACorrectingMachine/urls.py modified:   UACorrectingMachine/views.py

--- UACorrectingMachine/views.py
+++ UACorrectingMachine/views.py
@@ -8,10 +8,8 @@ def index(request):
         form = UACorrectingMachineSearchForm(request.POST)
         txt_for_correction = form.data.get("txt_for_correction")
         nn_numb_predicts = int(form.data.get("nn_numb_predicts"))
-        return render(request,
-                      'UACorrectingMachine/found.html',
-                      context={'result': predict_correction(text=txt_for_correction,
-                                                            num_return_sequences=nn_numb_predicts)})
+        return render_check(request, txt_for_correction, nn_numb_predicts)
+        
     else:
         form = UACorrectingMachineSearchForm
     return render(request,
@@ -22,3 +20,10 @@ def index(request):
 def about(request):
     return render(request,
                   'UACorrectingMachine/about.html')
+
+
+def render_check(request, text, nn_numb_predicts):
+    return render(request,
+                  'UACorrectingMachine/checked.html',
+                  context={'result': predict_correction(text=text,
+                                                        num_return_sequences=nn_numb_predicts)})
