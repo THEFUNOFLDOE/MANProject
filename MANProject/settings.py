f70d43ae96cc5ed90846b84b02912fab2de1f53d
Maksym <kovalchuka7105@gmail.com>
2023-05-14 19:02:42 +0300

    Final modified:   MANProject/settings.py new file:   MANProject/static/images/favicon.png modified:   UACorrectingMachine/static/UACorrectingMachine/images/instruction1.png modified:   UACorrectingMachine/static/UACorrectingMachine/images/instruction2.png modified:   UACorrectingMachine/static/UACorrectingMachine/images/instruction3.png modified:   static/UACorrectingMachine/images/instruction1.png modified:   static/UACorrectingMachine/images/instruction2.png modified:   static/UACorrectingMachine/images/instruction3.png new file:   static/images/favicon.png modified:   templates/UACorrectingMachine/base.html

--- MANProject/settings.py
+++ MANProject/settings.py
@@ -106,6 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = path.join(BASE_DIR, 'static/')
-STATICFILES_DIRS = [path.join(BASE_DIR, 'UACorrectingMachine/static')]
+STATICFILES_DIRS = [
+    path.join(BASE_DIR, 'MANProject/static'),
+    path.join(BASE_DIR, 'UACorrectingMachine/static')]
